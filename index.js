0a383be508b2c64db6f362face0e01ffa24a65d2
jagodaOP <119378430+jagodaOP@users.noreply.github.com>
2023-12-18 17:25:48 -0500

    Completed Challenges

--- index.js
+++ index.js
@@ -29,23 +29,30 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
+    for (let i=0; i < games.length; i++){
         // create a new div element, which will become the game card
-
-
+        const gameCard = document.createElement('div');
         // add the class game-card to the list
-
-
+        gameCard.classList.add('game-card');
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
-
+        gameCard.innerHTML = `
+            <img src="${games[i].img}" alt="${games[i].name}" class="game-img">
+            <h3>${games[i].name}</h3>
+            <p>Description: ${games[i].description}</p>
+            <p>Pledged: $${games[i].pledged}</p>
+            <p>Goal: $${games[i].goal}</p>
+            <p>Backers: ${games[i].backers}</p>
+        `;
         // append the game to the games-container
-
+        const gamesContainer = document.getElementById('games-container');
+        gamesContainer.appendChild(gameCard);
+    }
 }
+addGamesToPage(GAMES_JSON);
+
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
@@ -61,20 +68,18 @@ function addGamesToPage(games) {
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
-
-// set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+const totalContributions = GAMES_JSON.reduce((sum, game) => sum + game.backers, 0);
+contributionsCard.innerHTML = `${totalContributions.toLocaleString()}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
-
-// set inner HTML using template literal
-
+const totalRaised = GAMES_JSON.reduce((sum, game) => sum + game.pledged, 0);
+raisedCard.innerHTML = `$${totalRaised.toLocaleString()}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
-
+const numGames = GAMES_JSON.length;
+gamesCard.innerHTML = `${numGames}`;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
@@ -87,10 +92,10 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    const unfundedGames = GAMES_JSON.filter(game => game.goal > game.pledged);
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unfundedGames);
 }
 
 // show only games that are fully funded
@@ -98,10 +103,10 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    const fundedGames = GAMES_JSON.filter(game => game.goal <= game.pledged);
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(fundedGames);
 }
 
 // show all games
@@ -109,7 +114,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -118,7 +123,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
@@ -129,12 +136,16 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const unfundedGames = GAMES_JSON.filter(game => game.goal > game.pledged);
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const totalGames = GAMES_JSON.length;
+const fundraisingInfo = `Our company has raised $${totalRaised.toLocaleString()} across ${totalGames} games. ${unfundedGames.length > 0? `Currently, ${unfundedGames.length} games remain unfunded.`: "All games are fully funded!"} Explore our collection further below!`;
 
 // create a new DOM element containing the template string and append it to the description container
+const messageElement = document.createElement("p");
+messageElement.textContent = fundraisingInfo;
+descriptionContainer.appendChild(messageElement);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
@@ -149,7 +160,90 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [topGame, runnerUpGame, ... leftoverGames] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topGameNameElement = document.createElement("p");
+topGameNameElement.textContent = topGame.name;
+firstGameContainer.appendChild(topGameNameElement);
 
 // do the same for the runner up item
+const runnerUpGameNameElement = document.createElement("p");
+runnerUpGameNameElement.textContent = runnerUpGame.name;
+secondGameContainer.appendChild(runnerUpGameNameElement);
+
+/************************************************************************************
+ * Challenge 8 (Extra): Creating an our games button and animations for button clicks.
+ * When our games is clicked we will scroll down to our games and apply animations to show all games. 
+ * Same applies to the other buttons.
+ */
+
+// Remove the 'clicked' class from all buttons
+unfundedBtn.classList.remove('clicked');
+fundedBtn.classList.remove('clicked');
+allBtn.classList.remove('clicked');
+
+// Add event listeners to each button
+unfundedBtn.addEventListener("click", handleButtonClick);
+fundedBtn.addEventListener("click", handleButtonClick);
+allBtn.addEventListener("click", handleButtonClick);
+
+// Function to handle button click and initiate the animation
+function handleButtonClick(event) {
+  // Remove the 'clicked' class from all buttons
+  unfundedBtn.classList.remove('clicked');
+  fundedBtn.classList.remove('clicked');
+  allBtn.classList.remove('clicked');
+
+  // Add the 'clicked' class to the clicked button
+  event.target.classList.add('clicked');
+
+  // Scroll to the gamesContainer
+  document.getElementById('games-container').scrollIntoView({ behavior: 'smooth' });
+
+  // Prevent default behavior to avoid unnecessary page jumps
+  event.preventDefault();
+}
+
+// Function to handle scroll and update button appearance
+function handleScroll() {
+  const gamesContainer = document.getElementById('games-container');
+  const rect = gamesContainer.getBoundingClientRect();
+
+  // Check if the games container is in the viewport
+  if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+    // Remove the 'clicked' class from all buttons
+    unfundedBtn.classList.remove('clicked');
+    fundedBtn.classList.remove('clicked');
+    allBtn.classList.add('clicked'); // Add the 'clicked' class to the "Show All Games" button
+  } else {
+    // If not in the viewport, remove the 'clicked' class from the "Show All Games" button
+    allBtn.classList.remove('clicked');
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const ourGamesButton = document.createElement('button');
+    ourGamesButton.textContent = 'Our Games';
+    ourGamesButton.classList.add('scroll-button');
+  
+    ourGamesButton.addEventListener('click', function() {
+      // Remove the 'clicked' class from all buttons
+      document.getElementById('unfunded-btn').classList.remove('clicked');
+      document.getElementById('funded-btn').classList.remove('clicked');
+      document.getElementById('all-btn').classList.add('clicked'); // Add the 'clicked' class to the "Show All Games" button
+  
+      const ourGamesSection = document.getElementById('games-container');
+  
+      if (ourGamesSection) {
+        window.scrollTo({
+          top: ourGamesSection.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
+  
+    // Append the button to the header
+    document.querySelector('.header').appendChild(ourGamesButton);
+  });
+
